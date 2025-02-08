c21e85b1ea7dcdec81ece786517e6e5c08760040
ErickSwolkin <swolkinerick@gmail.com>
2025-02-08 22:22:45 +0100

    fet dividends_table

--- dividends/forms.py
+++ dividends/forms.py
@@ -5,18 +5,20 @@ class DividendForm(forms.ModelForm):
     
     class Meta:
         model = models.Dividend
-        fields = ["ticker", "value", "date", "quantity_quote"]
+        fields = ["ticker", "value", "date", "income_type"]
         
         widgets = {
             "ticker": forms.Select(attrs={"class": "form-control mt-2"}),
             "value": forms.NumberInput(attrs={"class": "form-control mt-2"}),
             "date": forms.DateInput(attrs={"class": "form-control mt-2"}),
-            "quantity_quote": forms.NumberInput(attrs={"class": "form-control mt-2", "placeholder": "Cálculo automático"})
-        }
+            #"quantity_quote": forms.NumberInput(attrs={"class": "form-control mt-2", "placeholder": "Cálculo automático"}),
+            "income_type": forms.Select(attrs={"class": "form-control mt-2"}),
+        }   
         
         labels = {
-            "ticker" : "nome do Ticker",
+            "ticker": "nome do Ticker",
+            "income_type": "Tipo de rendimento",
             "value": "Valor pago por conta", 
             "date": "Data de Pagamento", 
-            "quantity_quote": "quantidade de contas"
+            #"quantity_quote": "quantidade de contas"
         }
--- dividends/views.py
+++ dividends/views.py
@@ -1,4 +1,5 @@
-from itertools import groupby
+from django.db.models import Sum
+from collections import defaultdict
 from django.db.models.functions import ExtractMonth, ExtractYear
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -31,14 +32,23 @@ class DividendListView(ListView):
         context = super().get_context_data(**kwargs)
         
         anos = models.Dividend.objects.dates("date", "year", order="DESC")
+        meses = models.Dividend.objects.dates("date", "month", order="DESC")
         context["anos"] = [data.year for data in anos]
-        print(context["anos"])
+    
         context["meses"] = {
             1: "Janeiro", 2: "Fevereiro", 3: "Março", 4: "Abril",
             5: "Maio", 6: "Junho", 7: "Julho", 8: "Agosto",
             9: "Setembro", 10: "Outubro", 11: "Novembro", 12: "Dezembro"
         }
-
+        #agregando os dividendos
+        dividends_by_years_month = models.Dividend.objects.values("date__year", "date__month").annotate(total_value=Sum("total_value")).order_by("-date__year", "date__month")
+        dividends_dict = defaultdict(
+            lambda: {month: 0 for month in range(1, 13)})
+        for entry in dividends_by_years_month:
+            year = entry["date__year"]
+            month = entry["date__month"]
+            dividends_dict[year][month] = entry["total_value"]
+        context["dividends_by_year_and_month"] = dict(dividends_dict)
         return context
 
 class DividendCreateView(CreateView):
